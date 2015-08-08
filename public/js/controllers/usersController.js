430dd1b7fa35b12ae297ab4864a3f48b67154088
yazman12 <yazman12@gmail.com>
2015-08-08 10:49:40 -0400

    Added active ng-class for selected user

--- public/js/controllers/usersController.js
+++ public/js/controllers/usersController.js
@@ -14,7 +14,7 @@ function usersController ($http, $stateParams, userService) {
 	});
 
 	vm.showOneUser = function (user) {
-		console.log(user.etc);
+		console.log(this);
 		vm.oneUser = user;
 	};
 }
