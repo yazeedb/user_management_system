bbed05b5f4418bb27a85507b04404cf31ba134c5
yazman12 <yazman12@gmail.com>
2015-09-27 18:28:41 -0400

    Minor comma fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-        database: 'mongodb://test:test@ds051843.mongolab.com:51843/ums'
+        database: 'mongodb://test:test@ds051843.mongolab.com:51843/ums',
 	secret: 'ilovewebdev',
 	port: process.env.PORT || 8080
 };
