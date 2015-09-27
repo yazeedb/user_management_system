6f85f90a9e3cc33f0eb726d150f6dd82844445b4
yazman12 <yazman12@gmail.com>
2015-09-27 18:27:31 -0400

    Updated database from mongolab

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	database: 'mongodb://localhost/ums',
+        database: 'mongodb://test:test@ds051843.mongolab.com:51843/ums'
 	secret: 'ilovewebdev',
 	port: process.env.PORT || 8080
 };
